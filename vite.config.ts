3c963982293e54771013964ff49bd06050762706
SammyMa <sma990404@gmail.com>
2025-04-03 22:40:25 -0700

    fix: deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-memory-game/',
-  plugins: [vue()]
-})
+  base: process.env.VITE_BASE_URL || "/",
+  plugins: [vue()],
+});
